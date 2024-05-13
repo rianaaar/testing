eb355d52c426fbeb3d7b77550624ff5e7682afff
rianaaar <aar.riana10@gmail.com>
2024-05-13 18:50:31 +0700

    operator

---
+++ operator.js
@@ -0,0 +1,69 @@
+//operator aritmatika
+//const hasil = 1 + 2;
+//const hasil2 = 2 - 1;
+// const hasil3 = 2 * 1;
+// const hasil4 = 2 / 1;
+// const hasil5 = 2 ** 3;
+// const hasil6 = 5 % 2; //mod
+
+// console.log(`hasil: ${hasil}`);
+// console.log(`hasil2: ${hasil2}`);
+// console.log(`hasil3: ${hasil3}`);
+// console.log(`hasil4: ${hasil4}`);
+// console.log(`hasil5: ${hasil5}`);
+// console.log(`hasil6: ${hasil6}`);
+
+// let nilai = 2; 
+// nilai++; //menambah 1
+// nilai--; //mengurangi 1
+// console.log(`hasil7: ${nilai}`);
+
+// let hazil = (2 + 3) * 2;
+// console.log(`hazil: ${hazil}`);
+
+//operator penugasan: memasukan nilai kedalam variable
+// let nilai = 5;
+// //nilai = nilai + 2;
+// nilai += 2;
+// //nilai -= 2;
+// //nilai /= 2;
+// //nilai *= 2;
+// //nilai %= 2;
+// //nilai **= 2;
+// console.log(`nilai: ${nilai}`)
+
+//Perbandingan
+// const hasil = 5 > 2 ;
+// console.log(`hasil: ${hasil}`)
+// const hasil = 2 == '2' ; //apakah angka 2 samadengan string 2
+// const hasil2 = 2==='2' ; // bandingin tipe datanya juga
+// console.log(`hasil: ${hasil}`)
+// console.log(`hasil2: ${hasil2}`)
+// const hasil = 2 != '2' ; //apakah angka 2 samadengan string 2
+// const hasil2 = 2!=='2' ; // bandingin tipe datanya juga
+// console.log(`hasil: ${hasil}`)
+// console.log(`hasil2: ${hasil2}`)
+
+// LOGIKA
+// const hasil = 5>2 && 3>2; //harus bener keduanya maka true
+// const hasil2 = 5>2 || 3>2; //salah satu harus benar maka true
+// console.log(hasil)
+// const hasil = !5>2; //membalikan statetment hasilnya
+// console.log(hasil)
+
+//ifelse
+// if(1<3){
+//     console.log('true');
+// }else{
+//     console.log('salah');
+// }
+
+// //TERNARY
+// const hasil = 1 < 3 ? 'benar' : 'salah';
+// console.log(hasil)
+
+//Typeof -> mengetahui tipe data
+//kalau array & object & null = object
+//const nilai = null;
+// let nilai //akan undefined
+// console.log(typeof(nilai))
