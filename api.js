083c2357f7f8eec92d4d5bac2b54a71936c6d9fe
rianaaar <aar.riana10@gmail.com>
2024-06-27 13:40:29 +0700

    update tugas akhir

--- api.js
+++ api.js
@@ -30,6 +30,7 @@ const db = pgp({
 //          return buku;
 //   })
 
+// TUGAS AKHIR
 // get list semua buku
 //get list semua buku, bisa search by judul method : GET
 fastify.get('/buku', async function (request, reply) {
@@ -37,19 +38,24 @@ fastify.get('/buku', async function (request, reply) {
     const buku = await db.query("select * from buku where judul ilike $1",[`%${request.query.search}%`]
     );
       if (!buku || buku.length === 0) {
-        return reply.code(404).send({ message: 'Data tidak ditemukan' }); //kalau datanya ga ada
+        return reply.code(404).send({ message: 'Data dengan judul tersebut tidak ditemukan' }); //kalau data dengan judul yg dicari ga ada
       };
     return buku;
   }else{
-    const buku = await db.query("select * from buku");
+    const buku = await db.query("select * from buku"); //get all buku
          return buku;
   };  
 })
 // get detail buku by id (param id) method : GET
 fastify.get('/buku/:id', async function (request, reply) {
-  
-   const buku = await db.query("select * from buku where id = $1",[request.params.id]);
-         return buku;
+  if (!request.params.id) {
+    return reply.code(400).send({ message: 'ID is required' });
+  }
+  const buku = await db.query("select * from buku where id = $1",[request.params.id]);
+      if (!buku || buku.length === 0) {
+        return reply.code(404).send({ message: 'Data dengan id tersebut tidak ditemukan' });
+      }
+      return buku;
 })
 // insert data buku. method : POST
 fastify.post('/buku', async function (request, reply) {
@@ -71,6 +77,9 @@ fastify.post('/buku', async function (request, reply) {
    
 // update/edit buku by id(param). method: PUT
 fastify.put('/buku/:id', async function (request, reply) {
+  if (!request.params.id) {
+    return reply.code(400).send({ message: 'ID is required' });
+  }
   const buku = await db.query(
     "UPDATE buku SET sku = $1, judul = $2, harga = $3, stock = $4 WHERE id = $5 RETURNING *",
     [
@@ -81,6 +90,9 @@ fastify.put('/buku/:id', async function (request, reply) {
       request.params.id
     ]
   );
+  if (!buku || buku.length === 0) {
+    return reply.code(404).send({ message: 'Data dengan id tersebut tidak ditemukan' });
+  }
   //console.log(buku)
   reply
     .send({ message: 'Data berhasil diupdate', data: buku
@@ -96,7 +108,7 @@ fastify.delete('/buku/:id', async function (request, reply) {
     );
 
     if (!buku || buku.length === 0) {
-      return reply.code(404).send({ message: 'Data tidak ditemukan' });
+      return reply.code(404).send({ message: 'Data dengan id tersebut tidak ditemukan' });
     }
 
     reply
